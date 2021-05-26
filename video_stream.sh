117b526af29c82a1e084876feed78fac573373b0
wozza <warwick_stone@fastmail.com>
2021-05-26 12:50:00 +0100

    vid stream now good

--- video_stream.sh
+++ video_stream.sh
@@ -5,5 +5,14 @@
 # You can watch the stream in VLC by opening tcp/h264://pi.address:3333
 
 PORT=3333
+ROTATE_DEGREES=90
+FPS=10
+LOCAL_IP=`ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`
 
-raspivid -l -o tcp://0.0.0.0:$PORT -t 0
+echo View stream at tcp/h264://${LOCAL_IP:-whats.my.ip.address?}:$PORT
+
+raspivid -l -o tcp://0.0.0.0:$PORT -t 0 \
+    -rot $ROTATE_DEGREES \
+    -fps $FPS \
+    --exposure night \
+    --flicker 50hz
