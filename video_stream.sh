0d40e9bfb9ad6a2df71b25a31c0ddfcad0228e22
wozza <warwick_stone@fastmail.com>
2021-05-26 12:55:52 +0100

    video stream: add note about vlc playback

--- video_stream.sh
+++ video_stream.sh
@@ -6,6 +6,9 @@
 
 PORT=3333
 ROTATE_DEGREES=90
+# Note about fps: anything but default (25?) will cause VLC playback to
+# stutter. Configure VLC playback framerate via tools -> preferences ->
+# video -> input/codecs -> demuxers -> h264
 FPS=10
 LOCAL_IP=`ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`
 
