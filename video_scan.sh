362f1ec90143847df4b6574176d7c99d0039e768
Warwick Stone <warwick_stone@fastmail.com>
2021-05-29 11:26:45 +1000

    video scan: lower threshold

--- video_scan.sh
+++ video_scan.sh
@@ -6,8 +6,10 @@
 
 VIDEOS_DIR=/s/zz_woz_stuff/mousecam
 OUTPUT_DIR=/s/zz_woz_stuff/mousecam
+# MOTION_THRESHOLD. Lower = more sensitive. Default = 0.15
+MOTION_THRESHOLD=0.05
 
 for raw_vid in $VIDEOS_DIR/*.h264; do
     out_path=$OUTPUT_DIR/`basename $raw_vid`.mp4
-    dvr-scan -i $raw_vid -o $out_path
+    dvr-scan -i $raw_vid -o $out_path -t $MOTION_THRESHOLD
 done
