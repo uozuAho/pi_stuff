6c3010576eef491c69f1bf735ee4f60c7b8ef75d
Warwick Stone <warwick_stone@fastmail.com>
2021-08-28 18:27:54 +1000

    video scan: rename vars

--- video_scan.sh
+++ video_scan.sh
@@ -27,10 +27,9 @@ VIDEOS_DIR=/s/zz_woz_stuff/mousecam
 OUTPUT_DIR=/s/zz_woz_stuff/mousecam/scanned
 # Lower = more sensitive. Default = 0.15
 MOTION_THRESHOLD=0.05
-# delete the source video after scanning
-DELETE_AFTER_SCAN=yes
-# delete scanned outputs smaller than this threshold
-DELETE_SCANNED_THRESH=600k
+DELETE_SOURCE_AFTER_SCAN=yes
+# dvr-scan outputs small files, even if no motion was detected
+DELETE_OUTPUTS_SMALLER_THAN=600k
 
 # prevent processing 0 files (bad things happen)
 if [ `find $VIDEOS_DIR -iname "*.h264" | wc -l` -eq '0' ]; then
@@ -43,10 +42,10 @@ mkdir -p $OUTPUT_DIR
 for raw_vid in $VIDEOS_DIR/*.h264; do
     out_path=$OUTPUT_DIR/`basename $raw_vid`.mp4
     dvr-scan -i $raw_vid -o $out_path -t $MOTION_THRESHOLD
-    if [[ $DELETE_AFTER_SCAN == "yes" ]]; then
+    if [[ $DELETE_SOURCE_AFTER_SCAN == "yes" ]]; then
         rm $raw_vid
     fi
-    if [[ $DELETE_SCANNED_THRESH != "" ]]; then
-        find $out_path -size -$DELETE_SCANNED_THRESH -delete
+    if [[ $DELETE_OUTPUTS_SMALLER_THAN != "" ]]; then
+        find $out_path -size -$DELETE_OUTPUTS_SMALLER_THAN -delete
     fi
 done
